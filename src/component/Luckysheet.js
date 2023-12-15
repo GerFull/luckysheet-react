9d0a2aaaae0a63c71e6a9c9dc92d41c11721623b
German Petr <german.petya@mail.ru>
2023-12-15 10:44:10 +0500

    add setting

--- src/component/Luckysheet.js
+++ src/component/Luckysheet.js
@@ -2,11 +2,20 @@ import React from 'react';
 class Luckysheet extends React.Component {
 
     componentDidMount() {
+
+
         const luckysheet = window.luckysheet;
         luckysheet.create({
             container: "luckysheet",
-            plugins:['chart']
+            allowUpdate: true,
+            updateImageUrl: 'http://127.0.0.1:9004' + "/luckysheet/api/updateImg",
+            updateUrl: "ws://" + '127.0.0.1:9004' + "/luckysheet/websocket/luckysheet",
+            gridKey: "1079500#-8803#7c45f52b7d01486d88bc53cb17dcd2c3",
+            loadUrl: 'http://127.0.0.1:9004' + "/luckysheet/api/load",
+            loadSheetUrl: 'http://127.0.0.1:9004' + "/luckysheet/api/loadsheet"
         });
+
+        console.log(luckysheet)
     }
     render() {
         const luckyCss = {
@@ -20,8 +29,8 @@ class Luckysheet extends React.Component {
         }
         return (
             <div
-            id="luckysheet"
-            style={luckyCss}
+                id="luckysheet"
+                style={luckyCss}
             ></div>
         )
     }
